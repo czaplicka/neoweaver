fbac2928352c074ac206512ee5f9fe9fe1ce55bc
czaplicka <monika.czaplicka@gmail.com>
2026-03-08 08:13:12 +0100

    Create class-neoweaver-core.php

---
+++ includes/class-neoweaver-core.php
@@ -0,0 +1,14 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+class Neoweaver_Core {
+
+    public function __construct() {}
+
+    public function run() {
+        // Tu będziemy podpinać hooki, shortcody, system Supabase itd.
+    }
+}
