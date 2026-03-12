7748f4b135071f43cb70882f0503cca6fa29395d
czaplicka <monika.czaplicka@gmail.com>
2026-03-12 21:34:25 +0100

    Add files via upload

---
+++ assets/js/neoweaver-admin.js
@@ -0,0 +1,9 @@
+/* global jQuery */
+( function ( $ ) {
+	'use strict';
+
+	$( document ).ready( function () {
+		// Admin JS entry point.
+		console.log( 'NeoWeaver Admin loaded.' );
+	} );
+} )( jQuery );
---
+++ assets/js/neoweaver-public.js
@@ -0,0 +1,10 @@
+/* global jQuery, neoweaverData */
+( function ( $ ) {
+	'use strict';
+
+	$( document ).ready( function () {
+		// Public JS entry point.
+		// neoweaverData.ajaxUrl and neoweaverData.nonce are available here.
+		console.log( 'NeoWeaver Public loaded.' );
+	} );
+} )( jQuery );
