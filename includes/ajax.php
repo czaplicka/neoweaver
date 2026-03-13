783b94800442d18e044dd5225b547b3b62115c7e
czaplicka <monika.czaplicka@gmail.com>
2026-03-13 13:21:11 +0100

    feat(ajax): register tw-get-char-state.php in ajax router

--- includes/ajax.php
+++ includes/ajax.php
@@ -7,3 +7,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 require_once NEOWEAVER_PLUGIN_DIR . 'includes/ajax/tw-ensure-world-state.php';
 require_once NEOWEAVER_PLUGIN_DIR . 'includes/ajax/tw-get-session-state.php';
 require_once NEOWEAVER_PLUGIN_DIR . 'includes/ajax/tw-get-scenarios.php';
+require_once NEOWEAVER_PLUGIN_DIR . 'includes/ajax/tw-get-char-state.php';
