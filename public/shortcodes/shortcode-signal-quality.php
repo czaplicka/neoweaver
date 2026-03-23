c8145bea81e801d1cff378af5b436ba94f0cf895
czaplicka <monika.czaplicka@gmail.com>
2026-03-23 23:05:29 +0100

    Update shortcode-signal-quality.php

--- public/shortcodes/shortcode-signal-quality.php
+++ public/shortcodes/shortcode-signal-quality.php
@@ -1,4 +1,19 @@
 <?php
+// Przykład pobrania poziomu technologii (skala 1-5)
+$world_tech_level = $current_world['base_tech']; // Wartość z bazy Supabase
+
+// Mapowanie na procenty dla paska postępu
+$signal_strength = ($world_tech_level / 5) * 100;
+
+// Klasa CSS zależna od poziomu szumu
+$noise_class = '';
+if ($world_tech_level <= 2) {
+    $noise_class = 'high-interference';
+} elseif ($world_tech_level <= 4) {
+    $noise_class = 'stable-link';
+} else {
+    $noise_class = 'pure-signal';
+}
 add_shortcode('SIGNAL_QUALITY', function() use ($signal_strength, $world_tech_level) {
     ob_start(); ?>
     <div class="neoweave-signal-monitor">
