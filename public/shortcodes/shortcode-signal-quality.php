2e29c7cf50e3e3e0389e058dcd01a290a5d09c83
czaplicka <monika.czaplicka@gmail.com>
2026-03-23 18:40:12 +0100

    Create shortcode-signal-quality.php

---
+++ public/shortcodes/shortcode-signal-quality.php
@@ -0,0 +1,15 @@
+add_shortcode('SIGNAL_QUALITY', function() use ($signal_strength, $world_tech_level) {
+    ob_start(); ?>
+    <div class="neoweave-signal-monitor">
+        <div class="signal-label">SIGNAL INTEGRITY: <?php echo $world_tech_level; ?>/5</div>
+        <div class="signal-bar-container">
+            <div class="signal-bar-fill" style="width: <?php echo $signal_strength; ?>%;"></div>
+        </div>
+        <div class="signal-status"><?php 
+            if($world_tech_level <= 2) echo "STATUS: UNSTABLE / ANALOG INTERFERENCE DETECTED";
+            else echo "STATUS: ENCRYPTED LINK ESTABLISHED";
+        ?></div>
+    </div>
+    <?php
+    return ob_get_clean();
+});
