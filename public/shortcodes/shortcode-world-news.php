df0f3eeca8190c20fb38940a331bd471e81a491e
czaplicka <monika.czaplicka@gmail.com>
2026-03-24 14:48:30 +0100

    Update shortcode-world-news.php

--- public/shortcodes/shortcode-world-news.php
+++ public/shortcodes/shortcode-world-news.php
@@ -1,34 +1,35 @@
 function get_cyber_world_news_ajax() {
-    // 1. Sprawdzenie bezpieczeństwa i parametrów
-    $world_id = isset($_POST['world_id']) ? sanitize_text_field($_POST['world_id']) : null;
-    $player_clearance = isset($_POST['clearance']) ? intval($_POST['clearance']) : 0;
+    $world_id = sanitize_text_field($_POST['world_id']);
+    $character_id = sanitize_text_field($_POST['character_id']); // ID aktywnej postaci
+    $current_day = intval($_POST['current_day']);
+    $current_hour = intval($_POST['current_hour']);
 
-    if (!$world_id) {
-        wp_send_json_error('Missing World ID');
-    }
-
-    // 2. Pobranie danych z wp-config (zgodnie z Twoim opisem)
-    $supa_url = SUPABASE_URL; // Załóżmy, że masz te stałe zdefiniowane
+    $supa_url = SUPABASE_URL;
     $supa_key = SUPABASE_KEY;
 
-    // 3. Budowa zapytania do Supabase
-    // Filtry: ten konkretny świat, news aktywny, clearance mniejszy lub równy graczowi
-    $url = $supa_url . "/rest/v1/cyber_world_news?world_id=eq." . $world_id . "&is_active=eq.true&clearance_level=lte." . $player_clearance . "&order=created_at.desc";
+    // Filtry: ten świat, aktywne, oraz czas (tylko to, co już się wydarzyło w grze)
+    // lte = less than or equal (mniejsze lub równe obecnemu dniowi)
+    $url = $supa_url . "/rest/v1/cyber_world_news?world_id=eq.$world_id&game_day=lte.$current_day&is_active=eq.true&order=game_day.desc,game_hour.desc";
 
-    $response = wp_remote_get($url, array(
-        'headers' => array(
-            'apikey'    => $supa_key,
+    $response = wp_remote_get($url, [
+        'headers' => [
+            'apikey' => $supa_key,
             'Authorization' => 'Bearer ' . $supa_key
-        )
-    ));
+        ]
+    ]);
 
-    if (is_wp_error($response)) {
-        wp_send_json_error('Connection Error');
+    $news = json_decode(wp_remote_retrieve_body($response), true);
+
+    // Sprawdzamy, czy są nieprzeczytane
+    $unread_count = 0;
+    foreach ($news as &$item) {
+        $read_by = is_array($item['read_by']) ? $item['read_by'] : json_decode($item['read_by'], true);
+        $item['is_new'] = !in_array($character_id, (array)$read_by);
+        if ($item['is_new']) $unread_count++;
     }
 
-    $body = wp_remote_retrieve_body($response);
-    wp_send_json(json_decode($body));
+    wp_send_json([
+        'news' => $news,
+        'unread_count' => $unread_count
+    ]);
 }
-
-add_action('wp_ajax_get_cyber_news', 'get_cyber_world_news_ajax');
-add_action('wp_ajax_nopriv_get_cyber_news', 'get_cyber_world_news_ajax');
