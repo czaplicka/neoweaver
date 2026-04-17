28def45748640cace09fc082b89326c16745f5a6
czaplicka <monika.czaplicka@gmail.com>
2026-04-17 23:06:20 +0200

    character fix

--- includes/ajax.php
+++ includes/ajax.php
@@ -8,3 +8,4 @@ require_once NEOWEAVER_PLUGIN_DIR . 'includes/ajax/tw-ensure-world-state.php';
 require_once NEOWEAVER_PLUGIN_DIR . 'includes/ajax/tw-get-session-state.php';
 require_once NEOWEAVER_PLUGIN_DIR . 'includes/ajax/tw-get-scenarios.php';
 require_once NEOWEAVER_PLUGIN_DIR . 'includes/ajax/tw-get-char-state.php';
+require_once NEOWEAVER_PLUGIN_DIR . 'includes/ajax/tw-get-creator-data.php';
