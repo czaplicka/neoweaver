14df3af40abffa09fee5c32bad52a0968d60c815
czaplicka <monika.czaplicka@gmail.com>
2026-04-27 00:37:29 +0200

    Update shortcode-character-creator.php

--- public/shortcodes/shortcode-character-creator.php
+++ public/shortcodes/shortcode-character-creator.php
@@ -26,14 +26,14 @@ if ( ! function_exists( 'neoweaver_register_character_creator_assets' ) ) {
             $css_handle,
             $css_url,
             array(),
-            file_exists( $css_path ) ? (string) filemtime( $css_path ) : '1.0.10'
+            file_exists( $css_path ) ? (string) filemtime( $css_path ) : '1.0.11'
         );
 
         wp_register_script(
             $js_handle,
             $js_url,
             array(),
-            file_exists( $js_path ) ? (string) filemtime( $js_path ) : '1.0.6',
+            file_exists( $js_path ) ? (string) filemtime( $js_path ) : '1.0.7',
             true
         );
 
