ab0f69e170795461940a3ed32dda1640178535c5
czaplicka <monika.czaplicka@gmail.com>
2026-05-05 23:33:39 +0200

    Update neoweaver-wp-core.php

--- neoweaver-wp-core.php
+++ neoweaver-wp-core.php
@@ -316,5 +316,10 @@ final class NeoWeaver_Core {
 		<?php
 	}
 }
-
+wp_enqueue_style(
+    'font-awesome-6',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css',
+    [],
+    '6.5.2'
+);
 NeoWeaver_Core::init();
