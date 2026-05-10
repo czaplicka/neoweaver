05023b2eee1161a922f26ec4af83c15535b8b3a0
czaplicka <monika.czaplicka@gmail.com>
2026-05-10 14:17:41 +0200

    Define NW_PLUGIN_DIR for easier access

--- neoweaver-wp-core.php
+++ neoweaver-wp-core.php
@@ -15,6 +15,7 @@ define( 'NEOWEAVER_VERSION', '0.7.1' );
 define( 'NEOWEAVER_PLUGIN_FILE', __FILE__ );
 define( 'NEOWEAVER_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'NEOWEAVER_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'NW_PLUGIN_DIR', NEOWEAVER_PLUGIN_DIR);
 
 final class NeoWeaver_Core {
 
