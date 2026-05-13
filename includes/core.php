96911d1ab8900f097406cb3aeb73354f3958a99c
czaplicka <monika.czaplicka@gmail.com>
2026-05-13 16:50:51 +0200

    Update shortcode file includes in core.php

--- includes/core.php
+++ includes/core.php
@@ -15,13 +15,13 @@ class Neoweaver_Shortcodes_Core {  // <-- zmieniona nazwa
     public function register_shortcodes(): void {
         $base = plugin_dir_path( dirname( __FILE__ ) ) . 'public/shortcodes/';
 
-        require_once $base . 'shortcode-world-creator.php';
+        require_once $base . 'world-creator.php';
         add_shortcode( 'tw_world_creator', 'neoweaver_shortcode_world_creator' );
 
-        require_once $base . 'shortcode-character-creator.php';
+        require_once $base . 'character-creator.php';
         add_shortcode( 'tw_character_creator', 'neoweaver_shortcode_character_creator' );
 
-        require_once $base . 'shortcode-campaign-creator.php';
+        require_once $base . 'campaign-creator.php';
         add_shortcode( 'tw_campaign_creator', 'neoweaver_shortcode_campaign_creator' );
     }
 }
