ce6fafae64b0b954a06bf63eafb98fbf553a3d89
czaplicka <monika.czaplicka@gmail.com>
2026-05-13 21:07:52 +0200

    Add debug check for NeoWeaver Admin loading

--- assets/js/admin/admin.js
+++ assets/js/admin/admin.js
@@ -1,9 +1,10 @@
-/* global jQuery */
+/* global jQuery, neoweaverAdmin */
 ( function ( $ ) {
 	'use strict';
 
 	$( document ).ready( function () {
-		// Admin JS entry point.
-		console.log( 'NeoWeaver Admin loaded.' );
+		if ( window.neoweaverAdmin && window.neoweaverAdmin.debug ) {
+			console.log( 'NeoWeaver Admin loaded.' );
+		}
 	} );
 } )( jQuery );
