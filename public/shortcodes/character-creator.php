8c53d1847bf4b1024d490d19c818f7985277f48d
czaplicka <monika.czaplicka@gmail.com>
2026-05-16 23:33:47 +0200

    Remove unused asset path variables in character creator
    Removed unused variables for CSS and JS paths in the character creator.

--- public/shortcodes/character-creator.php
+++ public/shortcodes/character-creator.php
@@ -16,9 +16,6 @@ if ( ! function_exists( 'neoweaver_register_character_creator_assets' ) ) {
 		$css_handle = 'neoweaver-character-creator';
 		$js_handle  = 'neoweaver-character-creator';
 
-		$css_path = NEOWEAVER_PLUGIN_DIR . 'assets/css/public/character-creator.css';
-		$js_path  = NEOWEAVER_PLUGIN_DIR . 'assets/js/public/character-creator.js';
-
 		$css_url = NEOWEAVER_PLUGIN_URL . 'assets/css/public/character-creator.css';
 		$js_url  = NEOWEAVER_PLUGIN_URL . 'assets/js/public/character-creator.js';
 
@@ -26,14 +23,14 @@ if ( ! function_exists( 'neoweaver_register_character_creator_assets' ) ) {
 			$css_handle,
 			$css_url,
 			[],
-			file_exists( $css_path ) ? (string) filemtime( $css_path ) : '1.0.21'
+			(string) filemtime( NEOWEAVER_PLUGIN_DIR . 'assets/css/public/character-creator.css' )
 		);
 
 		wp_register_script(
 			$js_handle,
 			$js_url,
 			[],
-			file_exists( $js_path ) ? (string) filemtime( $js_path ) : '1.0.20',
+			(string) filemtime( NEOWEAVER_PLUGIN_DIR . 'assets/js/public/character-creator.js' ),
 			true
 		);
 
