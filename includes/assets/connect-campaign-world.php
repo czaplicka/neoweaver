27725c5abed670439805bb10451b37e230383d6f
czaplicka <monika.czaplicka@gmail.com>
2026-05-22 01:52:34 +0200

    Add connect-campaign-world.php for asset registration

---
+++ includes/assets/connect-campaign-world.php
@@ -0,0 +1,61 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! function_exists( 'tw_register_connect_campaign_world_assets' ) ) {
+	function tw_register_connect_campaign_world_assets(): void {
+		$css_handle = 'tw-deployment';
+		$js_handle  = 'tw-deployment';
+
+		$css_rel = 'assets/css/public/deployment.css';
+		$js_rel  = 'assets/js/public/deployment.js';
+
+		$css_path = NEOWEAVER_PLUGIN_DIR . $css_rel;
+		$js_path  = NEOWEAVER_PLUGIN_DIR . $js_rel;
+
+		wp_register_style(
+			$css_handle,
+			NEOWEAVER_PLUGIN_URL . $css_rel,
+			[],
+			file_exists( $css_path ) ? (string) filemtime( $css_path ) : NEOWEAVER_VERSION
+		);
+
+		wp_register_script(
+			$js_handle,
+			NEOWEAVER_PLUGIN_URL . $js_rel,
+			[],
+			file_exists( $js_path ) ? (string) filemtime( $js_path ) : NEOWEAVER_VERSION,
+			true
+		);
+	}
+}
+
+if ( ! function_exists( 'tw_enqueue_connect_campaign_world_assets' ) ) {
+	function tw_enqueue_connect_campaign_world_assets(): void {
+		static $done = false;
+
+		wp_enqueue_style( 'tw-deployment' );
+		wp_enqueue_script( 'tw-deployment' );
+
+		if ( $done ) {
+			return;
+		}
+
+		$done = true;
+
+		$config = [
+			'url' => function_exists( 'tw_supabase_url' ) ? trailingslashit( tw_supabase_url() ) : '',
+			'key' => function_exists( 'tw_supabase_anon_key' ) ? tw_supabase_anon_key() : '',
+			'uid' => get_current_user_id(),
+		];
+
+		wp_add_inline_script(
+			'tw-deployment',
+			'window.twDeploymentCfg = ' . wp_json_encode( $config ) . ';',
+			'before'
+		);
+	}
+}
+
+add_action( 'wp_enqueue_scripts', 'tw_register_connect_campaign_world_assets', 5 );
