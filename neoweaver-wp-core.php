6a936aba4bd13e25f40a28e4f27e2920207c66e6
czaplicka <monika.czaplicka@gmail.com>
2026-05-25 16:41:37 +0200

    Update neoweaver-wp-core.php

--- neoweaver-wp-core.php
+++ neoweaver-wp-core.php
@@ -63,6 +63,7 @@ final class NeoWeaver_Core {
 			'includes/classes/class-deployments-creator.php',
 			'includes/classes/class-nodes-creator.php',
 			'includes/classes/class-memory-parser.php',
+			'includes/classes/class-base-admin.php',
 
 						// Public
 			'public/class-public.php',
