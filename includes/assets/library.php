6b6b87f1569bc1edbb6e22224115fa9a18a6f0a5
czaplicka <monika.czaplicka@gmail.com>
2026-05-27 14:53:20 +0200

    feat: register and enqueue cards.css as dependency for library

--- includes/assets/library.php
+++ includes/assets/library.php
@@ -3,15 +3,26 @@ if ( ! defined( 'ABSPATH' ) ) { exit; }
 
 if ( ! function_exists( 'tw_register_library_assets' ) ) {
 	function tw_register_library_assets(): void {
-		$css_rel  = 'assets/css/public/library.css';
-		$js_rel   = 'assets/js/public/library.js';
-		$css_path = NEOWEAVER_PLUGIN_DIR . $css_rel;
-		$js_path  = NEOWEAVER_PLUGIN_DIR . $js_rel;
+		$cards_rel = 'assets/css/public/cards.css';
+		$css_rel   = 'assets/css/public/library.css';
+		$js_rel    = 'assets/js/public/library.js';
+		$cards_path = NEOWEAVER_PLUGIN_DIR . $cards_rel;
+		$css_path   = NEOWEAVER_PLUGIN_DIR . $css_rel;
+		$js_path    = NEOWEAVER_PLUGIN_DIR . $js_rel;
 
+		// Rejestruj cards.css (wspoldzielony komponent kart)
+		wp_register_style(
+			'neoweaver-cards',
+			NEOWEAVER_PLUGIN_URL . $cards_rel,
+			array(),
+			file_exists( $cards_path ) ? (string) filemtime( $cards_path ) : NEOWEAVER_VERSION
+		);
+
+		// library.css zalezy od cards.css
 		wp_register_style(
 			'neoweaver-library',
 			NEOWEAVER_PLUGIN_URL . $css_rel,
-			array(),
+			array( 'neoweaver-cards' ),
 			file_exists( $css_path ) ? (string) filemtime( $css_path ) : NEOWEAVER_VERSION
 		);
 
@@ -32,6 +43,7 @@ if ( ! function_exists( 'tw_enqueue_library_assets' ) ) {
 		static $done = false;
 
 		wp_enqueue_style( 'neoweaver-achievements' );
+		wp_enqueue_style( 'neoweaver-cards' );
 		wp_enqueue_style( 'neoweaver-library' );
 		wp_enqueue_script( 'neoweaver-library' );
 
