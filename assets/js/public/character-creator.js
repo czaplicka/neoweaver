b9f57acc8f97857a0703c9f8c4a73da98d17d470
czaplicka <monika.czaplicka@gmail.com>
2026-06-01 15:09:32 +0200

    Fix fetchPost call in loadPackages function

--- assets/js/public/character-creator.js
+++ assets/js/public/character-creator.js
@@ -1043,7 +1043,7 @@ window.requestAnimationFrame(function () {
 function loadPackages(classId) {
   if (!classId) { state.packages = []; renderPackages([]); return Promise.resolve([]); }
 
-  return fetchPost('neoweaver_get_starting_packages', { class_name: state.class_label.toLowerCase() })
+ return fetchPost('neoweaver_get_starting_packages', { class_name: state.class_label.toLowerCase() })
     .then(function (res) {
       var rows = res && res.success && Array.isArray(res.data) ? res.data : [];
       state.packages = rows;
