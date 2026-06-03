85b6162dba84ee8ed63d6283d7eb88e9868f919e
czaplicka <monika.czaplicka@gmail.com>
2026-06-03 17:36:56 +0200

    Define NW_UPLOADS_URL for uploads directory
    Add constant for uploads URL using WordPress upload directory.

--- neoweaver-wp-core.php
+++ neoweaver-wp-core.php
@@ -20,6 +20,7 @@ defined( 'NW_PLUGIN_FILE' ) || define( 'NW_PLUGIN_FILE', NEOWEAVER_PLUGIN_FILE )
 defined( 'NW_PLUGIN_DIR' )  || define( 'NW_PLUGIN_DIR', NEOWEAVER_PLUGIN_DIR );
 defined( 'NW_PLUGIN_PATH' ) || define( 'NW_PLUGIN_PATH', NEOWEAVER_PLUGIN_DIR );
 defined( 'NW_PLUGIN_URL' )  || define( 'NW_PLUGIN_URL', NEOWEAVER_PLUGIN_URL );
+define( 'NW_UPLOADS_URL', trailingslashit( wp_upload_dir()['baseurl'] ) );
 
 final class NeoWeaver_Core {
 
